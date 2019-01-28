1db0227adc9629ea7ad50b16e8e624c743237bcf
Bhargab Acharya <bhargab@seknox.com>
2019-01-28 20:03:08 +0545

    Updates

---
+++ precache-manifest.5a0b8af9b9b8cb6e0e5100f00cbac279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ded49a2979a7fef5d5b",
+    "url": "/mysite/static/js/runtime~main.3ded49a2.js"
+  },
+  {
+    "revision": "3bfac2e409bd2d16251e",
+    "url": "/mysite/static/js/main.3bfac2e4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mysite/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3bfac2e409bd2d16251e",
+    "url": "/mysite/static/css/main.3e5e9025.chunk.css"
+  },
+  {
+    "revision": "458ac092f56f09f7419bb59d29f60d3e",
+    "url": "/mysite/index.html"
+  }
+];
