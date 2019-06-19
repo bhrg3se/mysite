37dbc25645e0807d9542e86afc947e626758b3bf
Bhargab Acharya <bhargab@seknox.com>
2019-06-19 10:59:46 +0545

    gitlab cicd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import avatar from './avatar.png'
+import nown from './noun_tree.svg'
+import notBug from './notbug.gif'
 import './App.css';
+import { SocialIcon } from 'react-social-icons';
+import Avatar from 'react-avatar';
+import { Input, Menu, Button } from 'semantic-ui-react'
+import Particles from 'react-particles-js';
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
+
+            <Avatar  size="100" round={true} src={avatar}/>
+            <Particles
+                params={{
+                    particles: {
+                        line_linked: {
+                            shadow: {
+                                enable: true,
+                                color: "#3CA9D1",
+                                blur: 5
+                            }
+                        }
+                    }
+                }}
+                style={{
+                    width: '100%',
+                    //   backgroundImage: `url(${nown})`
+                }}
+            />
           <p>
-            Hello World
+            Hi
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <p>I am a full stack developer from Nepal.</p>
+            <p>Besides <a >programming</a> I love <a>movies</a>, <a>psychology</a> and <a>philosophy</a>  .</p>
+            <p>AND MEMES!!!</p>
+            
+          <div>
+              <SocialIcon url={"https://www.facebook.com/bhrg3se"} bgColor={"white"}/>
+              <SocialIcon url={"https://www.linkedin.com/bhrg3se"} bgColor={"white"}/>
+              <SocialIcon url={"https://www.twitter.com/bhrg3se"} bgColor={"white"}/>
+              <SocialIcon url={"https://www.github.com/bhrg3se"} bgColor={"white"}/>
+            
+
+          </div>
         </header>
+        <div>
+          <Button onClick={()=>{window.location="https://blog.bhargab.com.np"}}>Blog</Button>
+        </div>
       </div>
     );
   }
