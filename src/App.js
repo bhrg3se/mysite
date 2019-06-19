f8494977b3d1c9976f8422ccbd27927aa2fcd297
Bhargab Acharya <bhargab@seknox.com>
2019-06-19 11:32:04 +0545

    colors changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import avatar from './avatar.png'
-import nown from './noun_tree.svg'
 import notBug from './notbug.gif'
 import './App.css';
 import { SocialIcon } from 'react-social-icons';
 import Avatar from 'react-avatar';
 import { Input, Menu, Button } from 'semantic-ui-react'
-import Particles from 'react-particles-js';
 
 class App extends Component {
 
@@ -19,43 +17,28 @@ class App extends Component {
         <header className="App-header">
 
 
-            <Avatar  size="100" round={true} src={avatar}/>
-            <Particles
-                params={{
-                    particles: {
-                        line_linked: {
-                            shadow: {
-                                enable: true,
-                                color: "#3CA9D1",
-                                blur: 5
-                            }
-                        }
-                    }
-                }}
-                style={{
-                    width: '100%',
-                    //   backgroundImage: `url(${nown})`
-                }}
-            />
+            <Avatar  size="100" round={true} src={avatar} />
+
+            <img src={notBug}/>
           <p>
             Hi
           </p>
             <p>I am a full stack developer from Nepal.</p>
             <p>Besides <a >programming</a> I love <a>movies</a>, <a>psychology</a> and <a>philosophy</a>  .</p>
             <p>AND MEMES!!!</p>
-            
-          <div>
-              <SocialIcon url={"https://www.facebook.com/bhrg3se"} bgColor={"white"}/>
-              <SocialIcon url={"https://www.linkedin.com/bhrg3se"} bgColor={"white"}/>
-              <SocialIcon url={"https://www.twitter.com/bhrg3se"} bgColor={"white"}/>
-              <SocialIcon url={"https://www.github.com/bhrg3se"} bgColor={"white"}/>
+
+            <p>It ain't much but check out my <a href={"https://blog.bhargab.com.np"} style={{color:"darkgreen"}}>blog</a></p>
+          <div className={"Social-icons"} about={"Meh"}>
+              <SocialIcon url={"https://www.facebook.com/bhrg3se"} bgColor={"darkgreen"}/>
+              <SocialIcon url={"https://www.linkedin.com/bhrg3se"} bgColor={"darkgreen"}/>
+              <SocialIcon url={"https://www.twitter.com/bhrg3se"} bgColor={"darkgreen"}/>
+              <SocialIcon url={"https://www.github.com/bhrg3se"} bgColor={"darkgreen"}/>
             
 
           </div>
+
         </header>
-        <div>
-          <Button onClick={()=>{window.location="https://blog.bhargab.com.np"}}>Blog</Button>
-        </div>
+
       </div>
     );
   }
